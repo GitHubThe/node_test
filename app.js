483464ade03b7b5d0c7ee39be492a8ade7231163
huangg <huanggang@sp888.net>
2019-06-15 16:19:28 +0800

    舒适化

---
+++ app.js
@@ -0,0 +1,15 @@
+var http=require('http')
+var url = require('url')
+
+var server = http.createServer(function(req,res) {
+
+    var urlObj = url.parse(req.url)
+
+    res.writeHead(200,{"Content-Type":"text/plain;charset=UTF-8"});//纯文本
+
+    res.write("您好 nodejs" )
+
+    res.end()
+})
+
+server.listen(8003,"127.0.0.1") 
